3b40aed604e2a4a29de161e0485c1c17fe37e691
Marko <marko.kralik@gmail.com>
2020-12-14 19:56:32 +0100

    4 - Dynamic Capabilities

--- DataLayer/ContactRepositoryEx.cs
+++ DataLayer/ContactRepositoryEx.cs
@@ -16,11 +16,14 @@ namespace DataLayer
             this.db = new SqlConnection(connString);
         }
 
-
         public List<Contact> GetContactsById(params int[] ids)
         {
             return this.db.Query<Contact>("SELECT * FROM Contacts WHERE ID IN @Ids", new { Ids = ids}).ToList();
+        }
 
+        public List<dynamic> GetDynamicContactsById(params int[] ids)
+        {
+            return this.db.Query("SELECT * FROM Contacts WHERE ID IN @Ids", new { Ids = ids }).ToList();
         }
     }
 }
